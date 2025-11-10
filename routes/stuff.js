f70bb449781aafce010d374622351f4de95f4053
Thomas Berenger <berenger.thomas@gmail.com>
2025-11-10 15:49:54 +0100

    retour json POST & PUT

--- routes/stuff.js
+++ routes/stuff.js
@@ -2,22 +2,54 @@ const express = require("express");
 const router = express.Router();
 const Thing = require("../models/thing");
 
-router.post("/", (req, res, next) => {
-  // à cause du front end qui envoi deja un id
-  delete req.body._id;
-  const thing = new Thing({
-    ...req.body,
-  });
-  thing
-    .save()
-    .then(() => res.status(201).json({ messsage: "Objet enregistré ! " }))
-    .catch((error) => res.status(400).json({ error }));
+// router.post("/", (req, res, next) => {
+//   // à cause du front end qui envoi deja un id
+//   delete req.body._id;
+//   const thing = new Thing({
+//     ...req.body,
+//   });
+//   thing
+//     .save()
+//     .then(() => res.status(201).json({ messsage: "Objet enregistré ! " }))
+//     .catch((error) => res.status(400).json({ error }));
+// });
+
+router.post("/", async (req, res, next) => {
+  try {
+    delete req.body._id; // pour éviter un conflit si le front envoie déjà un id
+
+    const thing = new Thing({ ...req.body });
+    const savedThing = await thing.save();
+
+    // Retourne l'objet sauvegardé (tel qu'il est en DB)
+    res.status(201).json(savedThing);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 });
 
-router.put("/:id", (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, /* etre certains d'avoir le bon id pas celui du front de l'exo */ _id: req.body.id })
-    .then(() => res.status(200).json({ messsage: "Objet modifié ! " }))
-    .catch((error) => res.status(404).json({ error }));
+// router.put("/:id", (req, res, next) => {
+//   Thing.updateOne({ _id: req.params.id }, { ...req.body, /* etre certains d'avoir le bon id pas celui du front de l'exo */ _id: req.body.id })
+//     .then(() => res.status(200).json({ messsage: "Objet modifié ! " }))
+//     .catch((error) => res.status(404).json({ error }));
+// });
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const updatedThing = await Thing.findByIdAndUpdate(
+      req.params.id,
+      { ...req.body, _id: req.params.id }, // on force l'id du backend
+      { new: true } // renvoie le document modifié
+    );
+
+    if (!updatedThing) {
+      return res.status(404).json({ message: "Objet non trouvé !" });
+    }
+
+    res.status(200).json(updatedThing);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 });
 
 router.delete("/:id", (req, res, next) => {
